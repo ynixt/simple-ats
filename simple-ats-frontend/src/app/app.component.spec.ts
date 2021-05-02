242cc4e89dfb6c0ad0ed9ee5d47d26f44d1a1dfb
Gabriel Alves <bielvipbh@hotmail.com>
2021-05-01 22:46:44 -0300

    Adicionar specs para app component

--- simple-ats-frontend/src/app/app.component.spec.ts
+++ simple-ats-frontend/src/app/app.component.spec.ts
@@ -1,22 +1,87 @@
-import { async, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { HttpErrorResponse } from '@angular/common/http';
+import { fakeAsync, tick } from '@angular/core/testing';
+import { Spectator, SpyObject, createComponentFactory, mockProvider } from '@ngneat/spectator';
+import { TranslocoService } from '@ngneat/transloco';
+import { provideMockStore } from '@ngrx/store/testing';
+import { PoNotificationService } from '@po-ui/ng-components';
+import { BehaviorSubject, Subject } from 'rxjs';
 
-import { PoMenuModule, PoPageModule, PoToolbarModule } from '@po-ui/ng-components';
-
+import { TestUnsubscribeOnMethod } from 'src/test.commom.spec';
 import { AppComponent } from './app.component';
+import { TokenService } from './core/token.service';
+import { PermissionDispatchers, AuthDispatchers } from './store';
+import { PermissionState } from './store/reducers/permissions.reducer';
+import { PermissionsSelectors } from './store/services/selectors';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [PoMenuModule, PoPageModule, PoToolbarModule, RouterTestingModule],
-      declarations: [AppComponent],
-    }).compileComponents();
+  let component: AppComponent;
+  let spectator: Spectator<AppComponent>;
+
+  let translocoService: SpyObject<TranslocoService>;
+  let authDispatchers: SpyObject<AuthDispatchers>;
+  let tokenService: SpyObject<TokenService>;
+  let permissionDispatchers: SpyObject<PermissionDispatchers>;
+  let poNotificationService: SpyObject<PoNotificationService>;
+
+  const permissionsStateSubject = new Subject<PermissionState>();
+
+  const createComponent = createComponentFactory<AppComponent>({
+    component: AppComponent,
+    providers: [
+      ...provideMockStore(),
+      mockProvider(PermissionDispatchers),
+      mockProvider(PermissionsSelectors, { state$: permissionsStateSubject.asObservable() }),
+      mockProvider(TranslocoService),
+      mockProvider(AuthDispatchers),
+      mockProvider(TokenService),
+      mockProvider(PoNotificationService),
+    ],
+    detectChanges: false,
+  });
+
+  beforeEach(() => {
+    spectator = createComponent();
+    component = spectator.component;
+
+    translocoService = spectator.inject(TranslocoService);
+    authDispatchers = spectator.inject(AuthDispatchers);
+    tokenService = spectator.inject(TokenService);
+    permissionDispatchers = spectator.inject(PermissionDispatchers);
+    poNotificationService = spectator.inject(PoNotificationService);
+
+    translocoService.translate.and.callFake(key => key);
+    translocoService.selectTranslate.and.callFake(key => new BehaviorSubject(key).asObservable());
+    authDispatchers.loginByStorage.and.stub();
+    tokenService.tokenIsSaved.andReturn(true);
+    permissionDispatchers.getPermissions.and.stub();
+    poNotificationService.error.and.stub();
+
+    spectator.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('ngOnInit', () => {
+    expect(authDispatchers.loginByStorage).toHaveBeenCalled();
+    expect(tokenService.tokenIsSaved).toHaveBeenCalled();
+    expect(permissionDispatchers.getPermissions).toHaveBeenCalled();
+  });
+
+  it('permissionsSelectors.state$ with 401 error', fakeAsync(() => {
+    permissionsStateSubject.next({
+      loading: false,
+      error: new HttpErrorResponse({ status: 401 }),
+    });
+
+    tick();
+
+    expect(poNotificationService.error).toHaveBeenCalledWith('sessionExpired');
+    expect(translocoService.selectTranslate).toHaveBeenCalledWith('sessionExpired');
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-
-    expect(app).toBeTruthy();
+  it('ngOnDestroy', () => {
+    TestUnsubscribeOnMethod(component, 'permissionsSubscription', 'ngOnDestroy');
   });
 });
